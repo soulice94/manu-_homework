39b4a5cf6051de82f72e864dd06306117d271fe5
Pedro Arturo Flores Ramirez <pedro@MacBook-Pro-de-Pedro.local>
2019-04-28 13:32:00 -0500

    responsive logic in game

--- main.js
+++ main.js
@@ -12,16 +12,19 @@ function getRandom(limit) {
 
 // la acción del click del zombie
 function change_zombie() {
+    let canvas = document.getElementById("canvas");
     let zombie_container = document.getElementById("zombie");
     let final_time = Date.now();
     let currentScore = document.getElementById("currentScore");
     currentScore.innerText = `Score actual: ${(final_time - init_time) / 1000}s`;
     const index = Math.random() > 0.5 ? 0 : 1;
     zombie_container.src = gallery[index];
-    let final_position_x = `${getRandom(100)}%`;
-    let final_position_y = `${getRandom(100)}%`;
-    zombie_container.style.left = final_position_x;
-    zombie_container.style.top = final_position_y;
+    let final_position_x = getRandom(canvas.clientWidth);
+    let final_position_y = getRandom(canvas.clientHeight);
+    final_position_x = (final_position_x >= zombie_container.clientWidth) ? (final_position_x - zombie_container.clientWidth) : final_position_x;
+    final_position_y = (final_position_y >= zombie_container.clientHeight) ? (final_position_y - zombie_container.clientHeight) : final_position_y;
+    zombie_container.style.left = `${final_position_x}px`;
+    zombie_container.style.top = `${final_position_y}px`;
     console.log(final_position_x);
     console.log(final_position_y);
     init_time = Date.now();
@@ -30,4 +33,4 @@ function change_zombie() {
 setInterval(function(){
     let final_time = Date.now();
     document.getElementById("realTime").innerText = ((final_time - init_time)/1000) + "s";
-}, 20);
+}, 500);
